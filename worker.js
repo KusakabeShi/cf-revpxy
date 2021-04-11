b5e3cce278d9a6236bb96193edfcd965d229a300
日下部 詩 <kusakabesi2018@gmail.com>
2021-04-11 22:00:54 +0800

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,101 @@
+reverse = {}
+target = {}
+
+addEventListener("fetch", event => {
+  var url = new URL(event.request.url);
+  for(const [s_domain,s_target] of Object.entries(reverse)){
+    //   console.log(url.host)
+      if(url.host.endsWith(s_domain)){
+          target = reverse[s_domain];
+          target.f_host = s_domain;
+        //   console.log("Match: " + s_domain)
+          break;
+      }
+      else{
+        //   console.log("Not Match: " + s_domain)
+          continue;
+      }
+  }
+  
+    if(target.f_host == undefined){
+        return event.respondWith(new Response("Error"));
+    }
+
+  url.protocol = target.protocol;
+  url.host = target.host;
+  //event.passThroughOnException();
+  return event.respondWith(handleRequest(url));
+})
+
+function cfDecodeEmail(encodedString) {
+    var email = "", r = parseInt(encodedString.substr(0, 2), 16), n, i;
+    for (n = 2; encodedString.length - n; n += 2){
+    	i = parseInt(encodedString.substr(n, 2), 16) ^ r;
+		email += String.fromCharCode(i);
+    }
+    return email;
+}
+
+function cfEncodeEmail(email, key=0) {
+    var randomnumber = Math.floor(Math.random() * (99 - 11 + 1)) + 11;
+    if(key==0){
+        key = randomnumber;
+    }
+    out = key.toString(16).padStart(2,"0")
+    for(const c of email){
+        out += (c.charCodeAt(0)^key).toString(16).padStart(2,"0")
+    }
+ return out;
+} 
+
+async function handleRequest(req_url) {
+
+// console.log(target);
+
+  if (req_url.pathname in target.reverse){
+    // console.log("reverse " + req_url.pathname + " to " + target.reverse[req_url.pathname])
+    var response = await fetch(target.reverse[req_url.pathname]);
+  }
+  else{
+    // console.log("Default " + req_url.pathname);
+    var response = await fetch(req_url);
+  }
+  // const response = await fetch(req_url);
+  let contype = response.headers.get("Content-Type")
+  // console.log(req_url)
+
+  if(contype.includes("json") || contype.includes("html") || contype.includes("text") || contype.includes("javascript")){
+    var html = await response.text();
+    // console.log(html)
+    // var orig_html = html;
+
+    allemail = [...html.matchAll(new RegExp("data-cfemail=\"([a-z0-9]+)\"","g"))].concat([...html.matchAll(new RegExp("email-protection#([a-z0-9]+)\"","g"))])
+    // console.log(allemail)
+    replaces_add = {}
+    for(const [_,org_cf_email] of allemail){
+        org_cf_email_decode = cfDecodeEmail(org_cf_email)
+        for(const [rs, rd] of Object.entries(target.replaces)){
+            org_cf_email_decode = org_cf_email_decode.replaceAll(rs,rd);
+        }
+        org_cf_email_decode = org_cf_email_decode.replaceAll( target.host , target.f_host);
+        replaces_add[org_cf_email] = cfEncodeEmail(org_cf_email_decode)
+    }
+    // console.log(replaces_add)
+    target.replaces = {...target.replaces, ...replaces_add }
+    target.html = html;
+    // Simple replacement regex
+    for(const [rs, rd] of Object.entries(target.replaces)){
+    //   console.log("replace " + rs + " to " + rd)
+      html = html.replaceAll(rs,rd);
+    }
+    // console.log("replace " + target.host + " to " + target.f_host)
+    html = html.replaceAll( target.host , target.f_host);
+
+    // return modified response
+    return new Response( html ,{ headers: response.headers } ) 
+    // return new Response( "\n\n\n====1231123====123123=================\n\n\n" +  html ) 
+  }
+  else{
+    return response;
+  }
+}
